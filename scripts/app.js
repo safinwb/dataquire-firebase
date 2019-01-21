096a0e909c6d10a1b710cb6d5a416aab829f2345
Safi Amir <46702275+safinwb@users.noreply.github.com>
2019-01-21 23:35:22 +0400

    Added switchable tabs
    Using ng-show.

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,22 @@
 angular.module('app', [])
   .controller('appController', function($scope) {
-    $scope.name = "Not";
+    
+    $scope.name = "true";
+    $scope.myHome = true;
+   //$scope.myValue = "true";
     
     $scope.home= function(){
-        $scope.name = "Poop";
+        $scope.name = "false";
+        $scope.myHome = true;
+        $scope.mySensor = false;
+        
     }
     
+    $scope.sens = function(){
+        $scope.mySensor = true;
+        $scope.myHome = false;
+        
+    }
+    
+
 });
